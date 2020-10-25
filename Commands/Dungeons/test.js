50341eea6ca063e56c5d35daa1cf9316e94a87cf
unknown <bonkinans@gmail.com>
2020-10-25 21:29:41 +0200

    First Commit v1

--- Commands/Dungeons/test.js
+++ Commands/Dungeons/test.js
@@ -12,25 +12,10 @@ module.exports = class extends Command {
     });
 }
 async run(message) {
-    const canvas = Canvas.createCanvas(6500, 1150);
-    const ctx = canvas.getContext('2d');
-    const background = await Canvas.loadImage('./Commands/Dungeons/dlya_bonki1.png');
-    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-    ctx.strokeStyle = '#74037b';
-    ctx.strokeRect(0, 0, canvas.width, canvas.height);
-    ctx.font = '325px Helvetica Bold';
+    const a = new MessageEmbed()
+    .addField("<:Chest:769039250669699112>")
 
-    ctx.fillStyle = '#ffffff';
-    
-    ctx.fillText("Slime", canvas.width / 5.4, canvas.height / 2.1);
-
-	ctx.beginPath();
-	ctx.arc(450, 250, 75, 0, Math.PI * 2, true);
-	ctx.closePath();
-	ctx.clip();
-	const attachment = new MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
-
-    message.channel.send(attachment).then(msg => msg.delete({
+    message.channel.send(a).then(msg => msg.delete({
         "timeout": 900000
        }));
     }
