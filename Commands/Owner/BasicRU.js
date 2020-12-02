db7551e0b4216d0cc817b7040360e2e8172882c6
unknown <bonkinans@gmail.com>
2020-12-02 12:18:54 +0200

    Embeds

--- Commands/Owner/BasicRU.js
+++ Commands/Owner/BasicRU.js
@@ -16,9 +16,9 @@ module.exports = class extends Command {
 		const embed = new MessageEmbed()
         .setTitle(`**Server: ${message.guild.name}**`)
         .setColor('34B7EB')
-        .setDescription(`Basic`)
-        .addField("**Interesting & General**", "[Basic Information](https://biolog-n.gitbook.io/bro-igrayut-pererozhdenie/english/basic-information)\nTo find information on developing a server and value list, follow the link above.")
-        .addField("**Social network**", "<:01:774670675947421747> [YouTube](https://www.youtube.com/channel/UCH3I08n1RAz0AcOLZUJ5ujQ)\n<:03:774670676165918730> [VK](https://vk.com/bro_ytb)\n<:02:774670676581548112> [Instagram](https://www.instagram.com/bro_rbx)")
+        .setDescription(`Основное`)
+        .addField("**Интересное & Основное**", "[Основная информация](https://biolog-n.gitbook.io/bro-igrayut-pererozhdenie/osnovnaya-informaciya)\nДля нахождение информации по разработке сервера, валуе-листа и артам, перейдите по ссылке выше.")
+        .addField("**Социальные сети**", "<:01:774670675947421747> [YouTube](https://www.youtube.com/channel/UCH3I08n1RAz0AcOLZUJ5ujQ)\n<:03:774670676165918730> [VK](https://vk.com/bro_ytb)\n<:02:774670676581548112> [Instagram](https://www.instagram.com/bro_rbx)")
         .setFooter(`ID: ${message.guild.id} | BonMurBot ©️ 2020-2020 Все Права Съедены.`)
         message.channel.send("**Важное / The Important**" ,{embed: embed})
     }
--- Commands/Owner/MentionPingEng.js
+++ Commands/Owner/MentionPingEng.js
@@ -16,10 +16,14 @@ module.exports = class extends Command {
 		const embed = new MessageEmbed()
         .setTitle(`**Server: ${message.guild.name}**`)
         .setColor('34B7EB')
-        .setDescription(`Basic`)
-        .addField("**Interesting & General**", "[Basic Information](https://biolog-n.gitbook.io/bro-igrayut-pererozhdenie/english/basic-information)\nTo find information on developing a server and value list, follow the link above.")
+        .setDescription(`Mentions`)
+        .addField("**Mentions & Roles**", "📰 All The Main, 🎬 New Videos, 🎉 Giveaways\nTo get roles by mention, click on the reaction below corresponding to what you need.")
         .addField("**Social network**", "<:01:774670675947421747> [YouTube](https://www.youtube.com/channel/UCH3I08n1RAz0AcOLZUJ5ujQ)\n<:03:774670676165918730> [VK](https://vk.com/bro_ytb)\n<:02:774670676581548112> [Instagram](https://www.instagram.com/bro_rbx)")
         .setFooter(`ID: ${message.guild.id} | BonMurBot ©️ 2020-2020 Все Права Съедены.`)
-        message.channel.send("**Важное / The Important**" ,{embed: embed})
+        message.channel.send("**Важное / The Important**" ,{embed: embed}).then(async msg => {
+            await msg.react('📰')
+            await msg.react('🎬')
+            await msg.react('🎉')
+        })
     }
 }
