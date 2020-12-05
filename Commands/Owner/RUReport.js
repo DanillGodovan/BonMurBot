14dfeae4af012d31a2d229717900adab22c1c0be
unknown <bonkinans@gmail.com>
2020-12-05 19:53:03 +0200

    update embeds

--- Commands/Owner/RUReport.js
+++ Commands/Owner/RUReport.js
@@ -17,7 +17,7 @@ module.exports = class extends Command {
         .setTitle(`**Server: ${message.guild.name}**`)
         .setColor('34B7EB')
         .setDescription(`Жалобы & Нарушения`)
-        .addField("**Жалобы & Нарушения**", "Для подачи жалобы в этот чат, напишите \"!report <@пользователь> <жалоба>\" в чатах по общению или команд.\nПросьба не подавать попросту жалобы, иначе рискуете получить наказание.")
+        .addField("**Жалобы & Нарушения**", "Для подачи жалобы в этот чат, напишите \"!report <@пользователь> <жалоба> <изображение доказательств>\" в чатах по общению или команд.\nПросьба не подавать попросту жалобы, иначе рискуете получить наказание.")
         .addField("**Социальные сети / Social network**", "<:01:774670675947421747> [YouTube](https://www.youtube.com/channel/UCH3I08n1RAz0AcOLZUJ5ujQ)\n<:03:774670676165918730> [VK](https://vk.com/bro_ytb)\n<:02:774670676581548112> [Instagram](https://www.instagram.com/bro_rbx)")
         .setFooter(`ID: ${message.guild.id} | BonMurBot ©️ 2020-2020 Все Права Съедены.`)
         message.channel.send("**Основное / Main**" ,{embed: embed})
