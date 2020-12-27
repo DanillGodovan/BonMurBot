30f3bba23f4e1d8091a34a6b21f1bdceb8038c37
unknown <bonkinans@gmail.com>
2020-12-27 23:59:51 +0200

    Random love command

---
+++ Commands/Веселые/random-love.js
@@ -0,0 +1,28 @@
+const Command = require("../../Structures/Command");
+const mongoose = require("mongoose");
+const User = require("../../data/user");
+const Discord = require("discord.js");
+const config = require('../../BotConfig.json')
+
+module.exports = class extends Command {
+    constructor(...args) {
+        super(...args, {
+            aliases: ["любовь"],
+            description: "Случайная любовь",
+            category: "Веселые",
+            guildOnly: true,
+			nsfw: false,
+        });
+    }
+
+    async run(message, args) {
+       let randomMember = message.guild.members.cache.random().user
+
+       let embed = new Discord.MessageEmbed()
+       .setTitle("**Случайная Любовь**")
+       .setColor('34B7EB')
+       .setDescription(`:heart: Мне кажется... В вас влюблен ${randomMember}`)
+       .setThumbnail(`${randomMember.avatarURL()}`)
+       message.reply(embed)
+    }
+};
